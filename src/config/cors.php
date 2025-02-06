66104bb05a034b8ad52e2caf3967d15032c3b84e
ashdum <ashot.dumikyan@gmail.com>
2025-02-07 01:38:51 +0400

    adding debugging

--- src/config/cors.php
+++ src/config/cors.php
@@ -15,20 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['*'], // Лучше заменить на URL фронта
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
